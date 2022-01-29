2bbdc90870f417007d97c4a935e40934dab7a5f7
Hasan <hasanansari97650@gmail.com>
2022-01-29 20:54:17 +0530

    resolving problem

--- app.js
+++ app.js
@@ -5,37 +5,37 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const request = require("request");
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine","ejs");
+require("dotenv").config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
 const https = require("https");
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
 
     res.render("home");
 });
-app.get("/skills",function(req,res){
+app.get("/skills", function (req, res) {
     res.render("skills");
 });
 
-app.get("/experience",function(req,res){
+app.get("/experience", function (req, res) {
     res.render("experience");
 });
 
-app.get("/about",function(req,res){
+app.get("/about", function (req, res) {
     res.render("about");
 });
 
-app.get("/education",function(req,res){
+app.get("/education", function (req, res) {
     res.render("education");
 });
 
-app.get("/more",function(req,res){
+app.get("/more", function (req, res) {
     res.render("more");
 });
-app.get("/form",function(req,res){
+app.get("/form", function (req, res) {
     res.render("form")
 })
 
@@ -50,8 +50,8 @@ app.post("/form", function (req, res) {
     const zip = req.body.zip;
     const phonenumber = req.body.number;
     const message = req.body.message;
-    
-    
+
+
 
     const data = {
         members: [
@@ -68,7 +68,7 @@ app.post("/form", function (req, res) {
                         state: state,
                         zip: zip
                     },
-                    TEXT:message,
+                    TEXT: message,
                 }
             }
         ]
@@ -76,30 +76,30 @@ app.post("/form", function (req, res) {
 
     var jsonData = JSON.stringify(data);
 
-    const url = "https://us5.api.mailchimp.com/3.0/lists/978d5fe682";
+    const url = process.env.SECOND;
 
     const options = {
         method: "POST",
-        auth: "Hasan_1605:a5dbcda099f748717ee5dd061e7e7882-us5"
+        auth: process.env.FIRST
     }
 
     const request = https.request(url, options, function (response) {
-        if(response.statusCode == 200){
-            res.sendFile(__dirname + "/success.html"); 
+        if (response.statusCode == 200) {
+            res.sendFile(__dirname + "/success.html");
         }
-        else{
-            res.sendFile(__dirname + "/failure.html"); 
+        else {
+            res.sendFile(__dirname + "/failure.html");
         }
         response.on("data", function (data) {
             console.log(JSON.parse(data));
         });
-    
+
     });
 
     request.write(jsonData);
     request.end();
-    
+
 });
-app.listen(process.env.PORT||3001,function(){
+app.listen(process.env.PORT || 3001, function () {
     console.log("Server is running on port 3001");
 });
